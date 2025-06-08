de2c589683c7c30501f56611af6a824e44a4ca07
alekseyche <75145248+alekseyche@users.noreply.github.com>
2025-06-08 17:20:08 +0800

    Update Timer.h

--- Timer.h
+++ Timer.h
@@ -14,14 +14,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
-#include <string>
-#ifdef WIN64
 
 #ifndef TIMERH
 #define TIMERH
 
 #include <time.h>
 #include <string>
+#include <cstdint>
 #ifdef WIN64
 #include <windows.h>
 #endif
